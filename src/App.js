09f707e9e6a724cd096b69c86edc61f9e7121418
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2022-09-20 19:49:31 +0300

    Adding BrowserRouter,Route and Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
 import Dashboard from './pages/Dashboard';
 import TciketPage from './pages/TciketPage';
 const App = () => {
   return (
     <div className="app">
+      
       <BrowserRouter>
+      <Nav/>
+
         <Routes>
           <Route path='/' element={<Dashboard/>} />
           <Route path='/Tciket' element={<TciketPage/>} />
-
+          <Route path='/Tciket:id' element={<TciketPage editMode={true}/>} />
         </Routes>
+
       </BrowserRouter>
+
     </div>
   );
 }
